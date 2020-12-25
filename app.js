8b145cbcea5015426e8d7544a485ba7dbc959cf6
M. Francis <mfrancis48439@gmail.com>
2020-12-25 12:00:05 -0500

    Working Server
    A few things were added:
    - A 404 page (which is unused at the moment)
    - An empty favicon.ico file
    - A working server code thing! app.js is now working properly and can be used. The game is still offline play though

--- app.js
+++ app.js
@@ -1,43 +1,94 @@
 //Created by M. Francis - 2020
 
-const port = 8080;
+"use strict";
 
-var server = require('http').createServer(function(req, res){
-	switch(req.url){
-		//Main page
-		case "/":
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			break;
-		default:
-			req.addListener('end', function(){
-				//Return the correct file
-				fileServer.serve(req, res);
-			});
-			break;
-	}
-});
-var io = require('socket.io').listen(server);
-var static = require('node-static'); // for serving files
+const port = process.env.PORT || 8080;
 
-//Create a new server
-var fileServer = new static.Server('./');
+var http = require("http");   //Web server
+var fs = require("fs");       //File server
+var url = require("url");     //URL parsing
+var mmm = require('mmmagic'); //MIME type parsing
+var path = require('path');   //File path detection
+
+var magic = new mmm.Magic(mmm.MAGIC_MIME_TYPE);
+
+//A function that returns the 404 webpage contents
+function return404(){
+	fs.readFile("./404.html", function(err, data){
+		return data;
+	});
+}
 
 //The port to listen to
-server.listen(port);
+http.createServer(function(req, res){
+	//Parse url
+	var q = url.parse(req.url, true);
+	var filename = "." + q.pathname;
+	if(filename == "./"){
+		filename = "./index.html";
+	}
 
-//Prevents debug messages (comment it out to have debug messages)
-io.set('log level', 1);
+	//Get file type
+	var extname = path.extname(filename);
+	var contentType = "text/html";
+	switch(extname){
+		case ".css":
+			contentType = "text/css";
+			break;
+		case ".js":
+			contentType = "text/javascript";
+			break;
+		case ".json":
+			contentType = "application/json";
+			break;
+		case ".png":
+			contentType = "image/png";
+			break;
+		case ".svg":
+			contentType = "image/svg+xml";
+			break;
+		case ".ico":
+			contentType = "image/x-icon";
+			break;
+	}
+
+	//Get file
+	fs.readFile(filename, function(err, data){
+		//If we received an error
+		if(err){
+			//Send the user a 404 error
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.write("404 :(\n" + filename + " is not a real place.\n" + err + " is what to look in to");
+			return res.end();
+		}
+
+		//Return the desired webpage
+		try {
+			console.log("SUCCESS: " + contentType);
+			res.writeHead(200, {"Content-Type": contentType});
+		} catch {
+			console.log("ERROR: " + contentType);
+			res.writeHead(200, {"Content-Type": "text/plain"});
+		}
+		
+		res.write(data);
+		return res.end();
+	});
+}).listen(port);
+
+var io = require("socket.io")(http);
 
 /**
  * Listens for incoming connections from clients
  */
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection", function (socket) {
 	//LISTEN FOR EVENTS IN HERE
 
 	//Send information about joining
 	socket.on("join", function(data){
 		// 
 	});
+	//Send information about leaving
 	socket.on("leave", function(data){
 		//
 	});
